1c97693d7ee220ce7cb8d2539b97210290e2ce7a
zacharylithgow <zachary@nevereatalone.io>
2018-04-20 15:30:21 -0400

    -another try of .travis.yml script 20

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-npm-cli-login -u $1 -p $2 -e $3
+npm_user_name = $1
+npm_user_password = $2
+npm_user_email = $3
+aws_bucket = $4
+aws_access_id = $5
+aws_access_secret = $6
+
+npm-cli-login -u $npm_user_name -p $npm_user_password -e $npm_user_email
 npm install
 npm run dev-no-watch    # to generate the full dist/js/workwell.js file (no minimization/uglification)
 npm run prod    # to generate the minimized dist/js/workwell.min.js file and the map file associated
@@ -8,18 +15,20 @@ npm run prod    # to generate the minimized dist/js/workwell.min.js file and the
 # publish our newest version of the lib to npm
 npm publish
 
-#bucket="$1"
-
 # the version number of the library to deploy (retrieved from package.json)
-#version=$(cat package.json \
-#  | grep version \
-#  | head -1 \
-#  | awk -F: '{ print $2 }' \
-#  | sed 's/[",]//g' \
-#  | tr -d '[[:space:]]')
+version=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+export AWS_ACCESS_KEY_ID=$aws_access_id
+export AWS_SECRET_ACCESS_KEY=$aws_access_secret
+export AWS_DEFAULT_REGION=us-east-1
 
 # loop through all the files in dist/js (including the minified one)
-#for file in dist/js/* ; do
+for file in dist/js/* ; do
     # publish each file from dist/js to our CDN (S3 bucket here)
-#    aws s3 cp $file s3://$bucket/libs/workwell/$version/
-#done
+    aws s3 cp $file s3://$aws_bucket/libs/workwell/$version/
+done
