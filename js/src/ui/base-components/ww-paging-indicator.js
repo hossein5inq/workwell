9f4d63cab53ebf8836c341a75e8ef0446930f52d
zacharylithgow <zachary@nevereatalone.io>
2018-05-21 14:28:02 -0400

    FPM-555
    - Refactored the paging-indicator component so that it follows the new V2 architecture (classes and everything)

---
+++ js/src/ui/base-components/ww-paging-indicator.js
@@ -0,0 +1,55 @@
+const BaseComponent = require("./ww-base-component");
+
+class PagingIndicator extends BaseComponent {
+    constructor(pageCount = 3, selectedPage = 0) {
+        super("div");
+        this.setPageCount(pageCount);
+        this.setSelectedPage(selectedPage);
+        this.dots = [];
+
+        this.addClass("ww-paging-indicator");
+        this.el.setAttribute("data-page-count", this.pageCount);
+        this.el.setAttribute("data-selected-page", this.selectedPage);
+    }
+
+    setSelectedPage(selectedPage) {
+        if (0 < selectedPage < this.dots.length) {
+            for (let i = 0; i < this.dots.length; i++) {
+                this.dots[i].removeAttribute("selected");
+            }
+            this.dots[selectedPage].setAttribute("selected", "");
+            this.selectedPage = selectedPage;
+            this.el.setAttribute("data-selected-page", selectedPage);
+        }
+        return this;
+    }
+
+    setPageCount(pageCount) {
+        this.pageCount = pageCount;
+        this.el.setAttribute("data-page-count", pageCount);
+        this.el.innerHTML = "";
+        this.dots = [];
+        for (let i = 0; i < pageCount; i++) {
+            let dot = document.createElement("div");
+            BaseComponent.addClass(dot, "ww-paging-indicator__dot");
+            this.el.appendChild(dot);
+
+            if (i === this.selectedPage) {
+                dot.setAttribute("selected", "");
+            }
+
+            this.dots.push(dot);
+        }
+        return this;
+    }
+
+    getSelectedPage() {
+        return this.selectedPage;
+    }
+
+    getPageCount() {
+        return this.pageCount;
+    }
+}
+
+module.exports = PagingIndicator;
